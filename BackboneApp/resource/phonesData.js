d00c80573a59ce8cfcedd445c917a2819979cb73
Sanja Risteska <Sanja Risteska>
2017-01-15 22:17:16 +0100

    Add link attr to model instances

--- BackboneApp/resource/phonesData.js
+++ BackboneApp/resource/phonesData.js
@@ -3,30 +3,35 @@ var data = [
 	"model" : "Galaxy S7 Edge",
 	"price" : "510$",
 	"color" : "Grey",
-    "img" : "img/smartphone.png"
+    "img" : "img/smartphone.png",
+    "link" : "GalaxyS7Edge"
   },
   { "manufacturer" : "Apple",
 	"model" : "iPhone 7 32GB",
 	"price" : "820$",
 	"color" : "Black",
-    "img" : "img/smartphone.png"
+    "img" : "img/smartphone.png",
+    "link" : "iPhone7"
   },
   { "manufacturer" : "Samsung",
 	"model" : "Galaxy J7",
 	"price" : "240$",
 	"color" : "Grey",
-    "img" : "img/smartphone.png"
+    "img" : "img/smartphone.png",
+    "link" : "GalaxyJ7"
   },
   { "manufacturer" : "Huawei",
 	"model" : "P9",
 	"price" : "400$",
 	"color" : "Rose gold",
-    "img" : "img/smartphone.png"
+    "img" : "img/smartphone.png",
+    "link" : "HuaweiP9"
   },
   { "manufacturer" : "Nexus",
 	"model" : "5",
 	"price" : "300$",
 	"color" : "White",
-    "img" : "img/smartphone.png"
+    "img" : "img/smartphone.png",
+    "link" : "Nexus5"
   }
 ];
