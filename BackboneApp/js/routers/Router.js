93e0eb50908584799094efabd8d9817dd950efe9
Sanja Risteska <Sanja Risteska>
2017-01-22 23:02:51 +0100

    Change data retrieving in the router

--- BackboneApp/js/routers/Router.js
+++ BackboneApp/js/routers/Router.js
@@ -16,18 +16,13 @@ app.Router = Backbone.Router.extend({
     },
     
     showMoreInfoGalaxyS7Edge: function(){
-        var galaxyS7 = new app.phonePreview({
-            manufacturer : "Samsung",
-            model : "Galaxy S7 Edge",
-            price : "510$",
-            img : "img/samsungS7.png",
-            color : "Grey",
-            size : "5.5 inches",
-            weight : "157 g",
-            CPU : "Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo) Octa-core (4x2.3 GHz Mongoose & 4x1.6 GHz Cortex-A53)",
-            primary_camera : "12 MP, f/1.7, 26mm, phase detection autofocus, OIS, LED flash",
-            secondary_camera : "5 MP, 1/4.1' sensor size, 1.34 µm pixel size, f/1.7, 22mm, dual video call, Auto HDR"
-        });
+        var galaxyS7 = new app.phonePreview(
+        _.first(
+            _.filter(data,function(phone){
+            return phone.link === "GalaxyS7Edge";
+        })
+        )
+        );
         $("#allPhones").hide();
         var samsungS7Preview = new app.phonePreviewView({model : galaxyS7});
         $("#phonePreviewSection").html(samsungS7Preview.render().el);
@@ -35,19 +30,13 @@ app.Router = Backbone.Router.extend({
     },
     
     showMoreInfoiPhone7: function(){
-       var iPhone7 = new app.phonePreview({
-            manufacturer : "Apple",
-            model : "iPhone 7 32GB",
-            price : "820$",
-            color : "Black",
-            img : "img/iphone.png",
-            link : "iPhone7",
-            size : "4.7 inches",
-            weight : "138 g",
-            CPU : "Quad-core 2.34 GHz (2x Hurricane + 2x Zephyr)",
-            primary_camera : "12 MP, f/1.8, 28mm, phase detection autofocus, OIS, quad-LED (dual tone) flash",
-            secondary_camera : "7 MP, f/2.2, 32mm, 1080p@30fps, 720p@240fps, face detection, HDR, panorama"
-        });
+       var iPhone7 = new app.phonePreview(
+       _.first(
+            _.filter(data,function(phone){
+            return phone.link === "iPhone7";
+        })
+        )
+       );
         $("#allPhones").hide();
         var iPhonePreview = new app.phonePreviewView({model : iPhone7});
         $("#phonePreviewSection").html(iPhonePreview.render().el);
@@ -55,18 +44,13 @@ app.Router = Backbone.Router.extend({
     },
     
     showMoreInfoGalaxyJ7: function(){
-       var galaxyJ7 = new app.phonePreview({
-            manufacturer : "Samsung",
-            model : "Galaxy J7",
-            price : "240$",
-            color : "Grey",
-            size : "5.5 inches",
-            img : "img/samsungJ7.png",
-            weight : "170 g",
-            CPU : "Octa-core (4x1.6 GHz Cortex-A53 & 4x1.0 GHz Cortex-A53) Octa-core 1.6 GHz Cortex-A53",
-            primary_camera : "13 MP, f/1.9, 28mm, autofocus, LED flash",
-            secondary_camera : "5 MP, f/1.9, LED flash"
-        });
+       var galaxyJ7 = new app.phonePreview(
+       _.first(
+            _.filter(data,function(phone){
+            return phone.link === "GalaxyJ7";
+        })
+        )
+       );
         $("#allPhones").hide();
         var samsungJ7Preview = new app.phonePreviewView({model : galaxyJ7});
         $("#phonePreviewSection").html(samsungJ7Preview.render().el);
@@ -74,18 +58,13 @@ app.Router = Backbone.Router.extend({
     },
     
     showMoreInfoHuaweiP9: function(){
-        var HuaweiP9 = new app.phonePreview({
-            manufacturer : "Huawei",
-            model : "P9",
-            price : "400$",
-            color : "Rose gold  ",
-            size : "5.2 inches",
-            img : "img/huaweiP9.png",
-            weight : "144 g",
-            CPU : "Octa-core (4x2.5 GHz Cortex-A72 & 4x1.8 GHz Cortex-A53)",
-            primary_camera : "Dual 12 MP, f/2.2, 27 mm, Leica optics, phase detection autofocus, dual-LED (dual tone) flash",
-            secondary_camera : "8 MP, f/2.4, 1080p"
-        });
+        var HuaweiP9 = new app.phonePreview(
+        _.first(
+            _.filter(data,function(phone){
+            return phone.link === "HuaweiP9";
+        })
+        )
+        );
         $("#allPhones").hide();
         var huaweiPreview = new app.phonePreviewView({model : HuaweiP9});
         $("#phonePreviewSection").html(huaweiPreview.render().el);
@@ -93,18 +72,13 @@ app.Router = Backbone.Router.extend({
     },
     
     showMoreInfoNexus5: function(){
-       var Nexus5 = new app.phonePreview({
-            manufacturer : "LG",
-            model : "Nexus5",
-            price : "300$",
-            color : "White",
-            size : "4.95 inches",
-            img : "img/nexus5.png",
-            weight : "130 g",
-            CPU : "Quad-core 2.3 GHz Krait 400",
-            primary_camera : "8 MP, f/2.4, 30mm, autofocus, OIS, LED flash",
-            secondary_camera : "1.3 MP, f/2.4, 1/6' sensor size, 1.9µm pixel size"
-        });
+       var Nexus5 = new app.phonePreview(
+       _.first(
+            _.filter(data,function(phone){
+            return phone.link === "Nexus5";
+        })
+        )
+       );
         $("#allPhones").hide();
         var nexusPreview = new app.phonePreviewView({model : Nexus5});
         $("#phonePreviewSection").html(nexusPreview.render().el);
